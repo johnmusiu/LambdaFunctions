86dc0e6948fcf988cddd9404767204baecf3e56e
John Musiu <johnmusiu@gmail.com>
2021-03-29 01:17:02 +0300

    Enable CORS and set worker lambda to run every min

--- api/handler.js
+++ api/handler.js
@@ -6,12 +6,11 @@ AWS.config.setPromisesDependency(require("bluebird"));
 const dynamoDb = new AWS.DynamoDB.DocumentClient();
 
 module.exports.price = (event, context, callback) => {
-
   let params = {
     TableName: "price_table",
     ProjectExpression: "price",
     ScanIndexForward: false,
-    Limit: 1
+    Limit: 1,
   };
 
   const onScan = (err, data) => {
@@ -22,6 +21,10 @@ module.exports.price = (event, context, callback) => {
     } else {
       console.log(data);
       return callback(null, {
+        headers: {
+          "Access-Control-Allow-Origin": "*",
+          "Access-Control-Allow-Methods": "GET",
+        },
         statusCode: 200,
         body: JSON.stringify({
           price: data.Items[0].price,
