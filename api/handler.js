828a321d3d39c4f7c4fea21ee26710d398d099d6
John Musiu <johnmusiu@gmail.com>
2021-03-30 01:13:21 +0300

    only save 1 row for price in db, fix errors

--- api/handler.js
+++ api/handler.js
@@ -8,27 +8,27 @@ const dynamoDb = new AWS.DynamoDB.DocumentClient();
 module.exports.price = (event, context, callback) => {
   let params = {
     TableName: "prices_table",
+    Key: {
+      id: "a871e850-90c0-11eb-aa8f-31a14b25820c",
+    },
     ProjectExpression: "price",
-    ScanIndexForward: false,
-    Limit: 1,
   };
 
   const onScan = (err, data) => {
+    const headers = {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Methods": "GET",
+    };
     if (err) {
       return callback(null, {
-        error: JSON.stringify(err),
-        headers: {
-          "Access-Control-Allow-Origin": "*",
-          "Access-Control-Allow-Methods": "GET",
-        },
+        headers,
+        error: JSON.stringify(err.error),
+        statusCode: 500,
       });
     } else {
       console.log(data);
       return callback(null, {
-        headers: {
-          "Access-Control-Allow-Origin": "*",
-          "Access-Control-Allow-Methods": "GET",
-        },
+        headers,
         statusCode: 200,
         body: JSON.stringify({
           price: data.Items[0].price,
--- get_price_worker.js
+++ get_price_worker.js
@@ -2,6 +2,7 @@
 const https = require("https");
 const uuid = require("uuid");
 const AWS = require("aws-sdk");
+const id = "a871e850-90c0-11eb-aa8f-31a14b25820c";
 
 AWS.config.setPromisesDependency(require("bluebird"));
 const dynamoDb = new AWS.DynamoDB.DocumentClient();
@@ -9,32 +10,82 @@ const dynamoDb = new AWS.DynamoDB.DocumentClient();
 module.exports.getPrice = (event, context) => {
   https
     .get("https://api.bitfinex.com/v1/pubticker/btcusd", (response) => {
-      let data = "";
+      let res = "";
 
       response.on("data", (chunk) => {
-        data += chunk;
+        res += chunk;
       });
 
       response.on("end", function () {
-        let params = {
+        var params = {
           TableName: "prices_table",
-          Item: {
-            id: uuid.v1(),
-            price: JSON.parse(data).ask,
-            created_at: new Date().getTime().toString(),
+          Key: {
+            id: "a871e850-90c0-11eb-aa8f-31a14b25820c",
           },
         };
-        dynamoDb.put(params, (err, data) => {
+
+        dynamoDb.get(params, (err, data) => {
           if (err) {
-            console.log("Error", err);
+            console.error(
+              "Unable to read item. Error JSON:",
+              JSON.stringify(err, null, 2)
+            );
           } else {
-            console.log("Success", data);
+            console.log("GetItem succeeded:", JSON.stringify(data, null, 2));
+            if (data.Item) {
+              updateItem(JSON.parse(res).ask);
+            } else {
+              insertItem(JSON.parse(res).ask);
+            }
           }
         });
-        console.log(JSON.parse(data).ask);
       });
     })
     .on("error", (error) => {
       console.log(error.message);
     });
+};
+
+const insertItem = (price) => {
+  let params = {
+    TableName: "prices_table",
+    Item: {
+      id,
+      price,
+      created_at: new Date().getTime(),
+    },
+  };
+  dynamoDb.put(params, (err, data) => {
+    if (err) {
+      console.log("Error", err);
+    } else {
+      console.log("Success", data);
+    }
+  });
+};
+
+const updateItem = (price) => {
+  var params = {
+    TableName: "prices_table",
+    Key: {
+      id,
+    },
+    UpdateExpression: "set price = :p",
+    ExpressionAttributeValues: {
+      ":p": price,
+    },
+    ReturnValues: "UPDATED_NEW",
+  };
+
+  console.log("Updating the item...");
+  dynamoDb.update(params, function (err, data) {
+    if (err) {
+      console.error(
+        "Unable to update item. Error JSON:",
+        JSON.stringify(err, null, 2)
+      );
+    } else {
+      console.log("UpdateItem succeeded:", JSON.stringify(data, null, 2));
+    }
+  });
 };
